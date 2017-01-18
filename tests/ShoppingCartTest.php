156a8ad7c78a365a5a6542c6029426c0cef1bd69
cloud-wu <cloud77920@gmail.com>
2017-01-19 07:59:57 +0800

    第二個測試案例，第一集買了一本，第二集也買了一本，價格應為190 (紅燈)

--- tests/ShoppingCartTest.php
+++ tests/ShoppingCartTest.php
@@ -15,9 +15,20 @@ class ShoppingCartTest extends TestCase
 		$target = 100;
 
 		$ShoppingCart = new ShoppingCart([
-			'name'=>'哈利波特1', 
-			'price'=>'100', 
-			'count'=>'1',
+			['name'=>'哈利波特1', 'price'=>'100', 'count'=>'1']
+		]);
+		$result = $ShoppingCart->getPrice();
+
+		$this->assertEquals($target, $result);
+	}
+
+	public function test_第一集買了一本，第二集也買了一本，價格應為190()
+	{
+		$target = 190;
+
+		$ShoppingCart = new ShoppingCart([
+			['name'=>'哈利波特1', 'price'=>'100', 'count'=>'1'],
+			['name'=>'哈利波特2', 'price'=>'100', 'count'=>'1']
 		]);
 		$result = $ShoppingCart->getPrice();
 
