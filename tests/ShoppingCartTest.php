a2c45b44460d5c0ff9678754d1fc586e3edc66c8
cloudwu <cloudwu@cw.com.tw>
2017-01-19 10:25:14 +0800

    第五個測試案例，一次買了整套，一二三四五集各買了一本，價格應為375 (紅燈)

--- tests/ShoppingCartTest.php
+++ tests/ShoppingCartTest.php
@@ -63,5 +63,21 @@ class ShoppingCartTest extends TestCase
 
 		$this->assertEquals($target, $result);
 	}
+
+	public function test_一次買了整套，一二三四五集各買了一本，價格應為375()
+	{
+		$target = 375;
+
+		$ShoppingCart = new ShoppingCart([
+			new Book('哈利波特1', 100, 1),
+			new Book('哈利波特2', 100, 1),
+			new Book('哈利波特3', 100, 1),
+			new Book('哈利波特4', 100, 1),
+			new Book('哈利波特5', 100, 1),
+		]);
+		$result = $ShoppingCart->getPrice();
+
+		$this->assertEquals($target, $result);
+	}
 }
 
